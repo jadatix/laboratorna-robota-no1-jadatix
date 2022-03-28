72f8105ce9c107399b2ec2c9c193a06523710cf9
jadatix <andrijlasenko@gmail.com>
2022-03-28 12:36:22 +0300

    feedback and item init

---
+++ src/components/Feedback.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "../list.css"
+
+
+const FeedbackItem = (props) => {
+    
+}
+
+
+const Feedback = (props) => {
+    
+    return <div className="bg-box" >
+        {props.d.map((el, idx) => <div key={idx}><FeedbackItem data={el} /></div>)}
+    </div>
+
+}
